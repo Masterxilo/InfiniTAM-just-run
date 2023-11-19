2f7d2c1168e70dd1014edf2a880693abf0469564
Paul Frischknecht <paulfrischknechtmx@gmail.com>
2023-11-19 01:42:25 +0100

    cleanup

--- scripts/InfiniTAM.with-teddy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-trap 'echo FATAL ERROR EXIT CODE $? AT $0:$LINENO' ERR
-
-./download-teddy
-./InfiniTAM Teddy/calib.txt 'Teddy/Frames/%04i.ppm' 'Teddy/Frames/%04i.pgm'
